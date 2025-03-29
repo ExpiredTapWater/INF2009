1a11c76785b3adb10be7d06a87cf8785994f6758
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-30 05:04:09 +0800

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -1,14 +1,9 @@
-# ---------- Import Stuff ----------
+# ----------------- Import Stuff ------------------
 import os
-import struct
-import wave
-from datetime import datetime
-
-# ---------- PicoVoice ----------
 import pvporcupine
 from pvrecorder import PvRecorder
 
-# ---------- Setup Environment ----------
+# --------------- Setup Environment ---------------
 access_key = os.getenv("PICOVOICE_KEY")
 
 # ---------- Wake Word (Porcupine) Setup ----------
@@ -45,6 +40,7 @@ def setup_porcupine():
     print("Porcupine Ready")
     return porcupine, recorder, keywords_formatted
 
+# ---------- Main Function Start ----------
 def main():
 
     # Get porcupine instance
@@ -56,7 +52,7 @@ def main():
             result = porcupine.process(pcm)
 
             if result >= 0:
-                print(f"[{datetime.now()}] Detected '{keywords_formatted[result]}'")
+                print(f"Detected '{keywords_formatted[result]}'")
 
     except KeyboardInterrupt:
         print("Stopping...")
