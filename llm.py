2c223c0f18cae081c4bdbea1130a75e8e4c94c95
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 10:09:23 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,12 +5,7 @@ pllm = picollm.create(
     access_key=os.getenv("PICOVOICE_KEY"),
     model_path='./phi2-290.pllm')
 
-print("Running 1")
-res = pllm.generate(prompt='What is the intent here: Remind John to buy dinner on his way home',
-                    completion_token_limit=64)
-print(res.completion)
-
-print("Running 2")
-res = pllm.generate(prompt='Rewrite in 1st person: Remind John to buy dinner on his way home',
+print("Running")
+res = pllm.generate(prompt='Instruct: Rewrite the sentence to 1st person "Remind John to buy dinner on his way home"',
                     completion_token_limit=64)
 print(res.completion)
