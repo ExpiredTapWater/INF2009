a1e2e72cd49145c380c18391502311d328e3e455
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 12:40:46 +0800

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -77,7 +77,7 @@ def setup_cheetah():
 
     print("Initializing Cheetah")
     cheetah = create(access_key=KEY,
-                     endpoint_duration_sec=0.5,
+                     endpoint_duration_sec=0.2,
                      enable_automatic_punctuation=False)
     print("Cheetah Ready")
     
