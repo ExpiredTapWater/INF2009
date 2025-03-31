c2cc62f42bb8ffca932bf1a8d792ab44030055df
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 10:15:19 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -8,5 +8,17 @@ pllm = picollm.create(
 print("Running")
 res = pllm.generate(prompt='Instruct: Rewrite the sentence to 2nd person: "Remind John to buy dinner on his way home"\nOutput:',
                     completion_token_limit=64,
-                    stop_phrases=["\n","Exercise:"])
+                    stop_phrases=["Exercise:"])
+print(res.completion)
+
+prompt = (
+    "### Instruction:\n"
+    "Rewrite the following sentence to 2nd person.\n"
+    "### Input:\n"
+    "Remind John to buy dinner on his way home.\n"
+    "### Output:"
+)
+res = pllm.generate(prompt=prompt,
+                    completion_token_limit=64,
+                    stop_phrases=["Exercise:"])
 print(res.completion)
