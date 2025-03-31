d3509e0a0bdcdacf831034ab1b0dc71a0fb19a19
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 15:48:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ KEY = os.getenv("PICOVOICE_KEY")
 NLP = None
 LLM = None
 PROMPT = (
-    "Rewrite the sentence in second person. Only output the rewritten sentence:\n"
+    "Rewrite the sentence as though you are talking to the other person. Only output the rewritten sentence:\n"
     "{TEXT}"
 )
+#"Rewrite the sentence in second person. Only output the rewritten sentence:\n"
 
 # Called when client connects to the broker
 def on_connect(client, userdata, flags, rc):
