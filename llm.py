12cafc48b64cdd0cdad2a67a61f17472f6b32783
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 09:56:06 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,5 +5,5 @@ pllm = picollm.create(
     access_key=os.getenv("PICOVOICE_KEY"),
     model_path='./phi2-290.pllm')
 
-res = pllm.generate('Rewrite this text to 3rd person: "Can you ask John to buy Eggs"')
+res = pllm.generate('Rewrite this message to be from the recipient perspective: "Can you remind John to buy dinner on his way home"')
 print(res.completion)
