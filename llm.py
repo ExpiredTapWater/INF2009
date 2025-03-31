6b6bb2b7fb0b0f1878141ea48ed3443842b61574
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 10:11:14 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -6,6 +6,6 @@ pllm = picollm.create(
     model_path='./phi2-290.pllm')
 
 print("Running")
-res = pllm.generate(prompt='Instruct: Rewrite the sentence to 1st person "Remind John to buy dinner on his way home"',
+res = pllm.generate(prompt='Instruct: Rewrite the sentence to 2nd person "Remind John to buy dinner on his way home"\nOutput:',
                     completion_token_limit=64)
 print(res.completion)
