606cf3a88fe6d8bc59d1ec2ac7d6003e2e644a29
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 09:59:57 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,5 +5,6 @@ pllm = picollm.create(
     access_key=os.getenv("PICOVOICE_KEY"),
     model_path='./phi2-290.pllm')
 
-res = pllm.generate('How are you?')
+res = pllm.generate(prompt='Rewrite in 3rd person: Remind John to buy dinner on his way home',
+                    completion_token_limit=64)
 print(res.completion)
