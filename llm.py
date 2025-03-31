aedfddabd817907c9efc1788bd3b79eec49d9155
ExpiredTapWater <2302822@sit.singaporetech.edu.sg>
2025-03-31 10:21:57 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -3,10 +3,10 @@ import picollm
 
 pllm = picollm.create(
     access_key=os.getenv("PICOVOICE_KEY"),
-    model_path='./phi2-290.pllm')
+    model_path='./phi3.5-289.pllm')
 
 print("Running")
 res = pllm.generate(prompt='Rewrite the sentence to 2nd person: Remind John to buy dinner on his way home',
-                    completion_token_limit=64,
+                    completion_token_limit=128,
                     stop_phrases=["Exercise:"])
 print(res.completion)
